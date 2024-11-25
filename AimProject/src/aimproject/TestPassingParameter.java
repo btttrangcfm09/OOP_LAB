43d2bc8777d3d108e12409438ffd37e53fa286c1
trangbui1209 <139475731+btttrangcfm09@users.noreply.github.com>
2024-11-25 21:00:16 +0700

    Passing parameter

--- AimProject/src/aimproject/TestPassingParameter.java
+++ AimProject/src/aimproject/TestPassingParameter.java
@@ -15,19 +15,21 @@ public class TestPassingParameter {
 		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
 		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 		
-//		swap(jungleDVD,cinderellaDVD);
-//		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-//		System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
-//		
-//		changeTitle(jungleDVD, cinderellaDVD.getTitle());
-//		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		swap(jungleDVD,cinderellaDVD);
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 		
-		// Test for swap function
+		// Test for correct swap function
         Wrapper wrapper1 = new Wrapper(jungleDVD);
         Wrapper wrapper2 = new Wrapper(cinderellaDVD);
 		swap(wrapper1, wrapper2);
 		System.out.println("jungle dvd title: " + wrapper1.value.getTitle());
-		System.out.println("cinderella dvd title: " + wrapper2.value.getTitle());		
+		System.out.println("cinderella dvd title: " + wrapper2.value.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		
+		
 		
 	}
 	public static void swap(Object o1, Object o2) {
