57c7ea3f3831d4872e6797ae41e9792a6ebe9297
trangbui1209 <139475731+btttrangcfm09@users.noreply.github.com>
2024-12-01 10:51:20 +0700

    Update Disc.java

--- AimProject/src/hust/soict/trang/aims/media/Disc.java
+++ AimProject/src/hust/soict/trang/aims/media/Disc.java
@@ -1,49 +1,48 @@
 package hust.soict.trang.aims.media;
 
 public class Disc extends Media{
+	// Contructor
 	public Disc(String title, String category, String director, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
+		super(title,category,cost);
 		this.director = director;
-		this.cost = cost;
 	}	
 	public Disc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
+		super(title,category,cost);
 	}
 	
 	public Disc(String title, String category, String director, int length, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
+		super(title,category,cost);
 		this.director = director;
 		this.length = length;
-		
-		this.cost = cost;
 	}
 	public Disc(String title, String category, int length,float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
+		super(title,category,cost);
 		this.length = length;
 	}
 	public Disc(String title) {
-		super();
-		this.title = title;
+		super(title);
 	}
+	public Disc(String title, String category) {
+		super(title,category);
+	}
+	public Disc(String title, float cost) {
+		super(title,cost);
+	}
+	public Disc(String title,String category,String director) {
+		super(title,director);
+		this.director = director;
+	}
+	// variable
 	protected String director;
 	protected int length;
+	// getter
 	public String getDirector() {
 		return director;
 	}
-	
 	public int getLength() {
 		return length;
 	}
+
 	
 	
 	
