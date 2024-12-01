78eb134393582a7e4d2f137d7dc37d6da084d954
trangbui1209 <139475731+btttrangcfm09@users.noreply.github.com>
2024-12-01 14:24:58 +0700

    Update Track.java

--- AimProject/src/hust/soict/trang/aims/media/Track.java
+++ AimProject/src/hust/soict/trang/aims/media/Track.java
@@ -24,6 +24,9 @@ public class Track implements Playable{
 	public void play() {
 		System.out.println("Playing Track: " + this.getTitle());
 		System.out.println("Track length: " + this.getLength());
+		if(this.getLength() == 0) {
+			System.out.println("the track cannot be played.");
+		}
 	}
 	
 	
