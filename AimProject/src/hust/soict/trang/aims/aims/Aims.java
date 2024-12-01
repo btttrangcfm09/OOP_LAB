b2e4aca5b386e181576122d1ebd5885ef460a075
trangbui1209 <139475731+btttrangcfm09@users.noreply.github.com>
2024-12-01 09:38:18 +0700

    update-store

--- AimProject/src/hust/soict/trang/aims/aims/Aims.java
+++ AimProject/src/hust/soict/trang/aims/aims/Aims.java
@@ -13,15 +13,15 @@ public class Aims {
 //		// Create new dvd object and add them to the cart
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
 				"Animation", "Roger Allers", 87, 19.95f);
-		anOrder.addDigitalVideoDisc(dvd1);
+		anOrder.addMedia(dvd1);
 		System.out.println(dvd1.getId());
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Start Wars", 
 				"Science Fiction", "George Luscas", 87, 24.95f);
-		anOrder.addDigitalVideoDisc(dvd2);
+		anOrder.addMedia(dvd2);
 		System.out.println(dvd2.getId());
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
 				"Animation", 18.99f);
-		anOrder.addDigitalVideoDisc(dvd3);
+		anOrder.addMedia(dvd3);
 		System.out.println(dvd3.getId());
 
 		
@@ -30,7 +30,7 @@ public class Aims {
 		System.out.println(anOrder.totalCost());
 		
 		// remove dvd in the List
-		anOrder.removeDigitalVideoDisc(dvd3);
+		anOrder.removeMedia(dvd3);
 		
 		// print the list of video after removing
 		anOrder.print();
@@ -44,7 +44,7 @@ public class Aims {
 		DigitalVideoDisc dvd6 = new DigitalVideoDisc("Aladin 3",
 				"Animation", 20.99f);
 		DigitalVideoDisc dvList[] = {dvd4,dvd5,dvd6};
-		anOrder.addDigitalVideoDisc(dvList);
+		anOrder.addMedia(dvList);
 		
 		// Test for add arbitrary number of arguments
 		DigitalVideoDisc dvd7 = new DigitalVideoDisc("The Flower 1",
@@ -53,14 +53,14 @@ public class Aims {
 				"Science Fiction", "George Luscas", 101, 25.95f);
 		DigitalVideoDisc dvd9 = new DigitalVideoDisc("Aladin 4",
 				"Animation", 30.99f);
-		anOrder.addDigitalVideoDisc(dvd7, dvd8,dvd9);
+		anOrder.addMedia(dvd7, dvd8,dvd9);
 //		anOrder.displayDigitalVideoDisc();
 		// Test for add differing the number of parameters
 		DigitalVideoDisc dvd10 = new DigitalVideoDisc("The Flower 2",
 				"Animation", "Roger Alin", 200, 30.05f);
 		DigitalVideoDisc dvd11 = new DigitalVideoDisc("Start Wars 4", 
 				"Science Fiction", "George Luscas", 300, 60.95f);
-		anOrder.addDigitalVideoDisc(dvd8, dvd9);
+		anOrder.addMedia(dvd8, dvd9);
 		anOrder.print();
 	}
 
