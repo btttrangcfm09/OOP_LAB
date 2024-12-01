83edca04e50778a25a21ffddc6d58af03c72dce4
trangbui1209 <139475731+btttrangcfm09@users.noreply.github.com>
2024-12-01 10:56:40 +0700

    Update Media.java

--- AimProject/src/hust/soict/trang/aims/media/Media.java
+++ AimProject/src/hust/soict/trang/aims/media/Media.java
@@ -1,6 +1,26 @@
 package hust.soict.trang.aims.media;
 
 public abstract class Media {
+	public Media(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	public Media(String title, String category) {
+		super();
+		this.title = title;
+		this.category = category;
+	}
+	public Media(String title, float cost) {
+		super();
+		this.title = title;
+		this.cost = cost;
+	}
+	public Media(String title) {
+		super();
+		this.title = title;
+	}
 	int id;
 	String title;
 	String category;
@@ -29,5 +49,15 @@ public abstract class Media {
 	public void setCost(float cost) {
 		this.cost = cost;
 	}
-	
+	public boolean isMatch(String t) {
+		for(int i = 0; i < t.length(); ++i) {
+			char c = t.charAt(i);
+			char c1 = Character.toLowerCase(c);
+			char c2 = Character.toUpperCase(c);
+			if(title.indexOf(c1)!=-1 || title.indexOf(c2) != -1) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
